c13e5cf47b49f1a6f8601e3f4716565adf337e16
Reales <rclarence123@gmail.com>
2023-03-08 17:47:57 -0500

    Se crean los modelos para consumo de API

---
+++ lib/models/models.dart
@@ -0,0 +1,2 @@
+export 'package:yofopelis/models/now_playing_response.dart';
+export 'package:yofopelis/models/movie.dart';
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:yofopelis/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '66311435b5ddfea993a5c2cfdb55b4c4';
@@ -22,7 +23,8 @@ class MoviesProvider extends ChangeNotifier {
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
 
-    final Map<String, dynamic> decodedData = json.decode(response.body);
-    print(decodedData['results']);
+    final nowPlayingResponse = NowPlayingResponse.fromRawJson(response.body);
+
+    print(nowPlayingResponse.results[0].title);
   }
 }
